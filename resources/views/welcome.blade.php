834dd535d423d150febc250a2aa7ccab6ac6ffeb
NataliyaShevchuk <nata.shevchuk@icloud.com>
2023-03-10 09:01:04 +0100

    add c-s vad

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 @section('content')
-
+    <section class="container">
+        <div class="d-flex justify-content-center align-items-center">
+            <div class="py-5">
+                <h1 class="text-white  align-middle">Welcome to our Delivery App!</h1>
+            </div>
+        </div>
+    </section>
 
 @endsection
